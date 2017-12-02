f4722dae4cb68bed1edcdda592aec82c13db290e
Peter Denno <podenno@gmail.com>
2017-12-02 16:54:57 -0500

    commit everything!

---
+++ .emacs.d/elpa/company-math-1.2/company-math-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "company-math" "1.2" "Completion backends for unicode math symbols and latex tags" '((company "0.8.0") (math-symbol-lists "1.2")) :commit "2cb03c48f44a5b3cbbbbe05e9841b2c61bd8ed81" :url "https://github.com/vspinu/company-math" :keywords '("unicode" "symbols" "completion"))
